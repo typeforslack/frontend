8cc4b9bac2849868e24fefd91d00801a720c75f1
aravind289 <aravindvitkrishnan@gmail.com>
2020-05-05 18:49:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as Login from './components/loginSignup'
+
+function App() {
+  return (
+    <div>
+      <Login.Signup />
+    </div>
+  );
+}
+
+export default App;
