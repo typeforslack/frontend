87f1c6b8ea60a1723734e2ef3b68924acc9f0916
vishnudut <vvdut99@gmail.com>
2020-06-14 21:42:20 +0530

    Basic Nav and Card

--- src/components/dashboard/card.jsx
+++ src/components/dashboard/card.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import './card.css'
 
 export default class Card extends Component {
   state = {}
 
   render() {
-    return(
-      <>
-      </>
+    return (
+      <div className="card-container">
+        <br />
+        <h1>WPM</h1>
+      </div>
     )
   }
 }
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
 import LineChart from '../common/lineChart'
 // import Card from "./card";
+import Card from './card'
+import { Navbar, Nav, NavDropdown, Container, Row, Col } from 'react-bootstrap'
+// import { LineChart } from './lineChart'
 
 export default class Dashboard extends Component {
   state = {}
 
   render() {
-    return <LineChart width={460} height={400} />
+
+    return (
+      <div>
+        <Nav fill variant="tabs" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">TypeForSlack</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Practice</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Arcade</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-3">Challange</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <br />
+
+        <Container>
+          <Row>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+          </Row>
+        </Container>
+        <LineChart width={460} height={400} />
+      </div>
+    )
   }
 }
